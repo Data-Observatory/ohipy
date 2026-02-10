57374f52f1aee010a37263be0b739cacf2cf6440
Developer <dev@example.com>
2026-02-10 16:15:12 -0300

    feat(goals): copy goals __init__

---
+++ src/ohipy/goals/__init__.py
@@ -0,0 +1,41 @@
+"""OHI Goals Module - Goal-specific calculation functions."""
+
+from .fis import FIS
+from .mar import MAR
+from .fp import FP
+from .ao import AO
+from .np import NP
+from .cs import CS
+from .cp import CP
+from .tr import TR
+from .liv import LIV
+from .eco import ECO
+from .le import LE
+from .ico import ICO
+from .lsp import LSP
+from .sp import SP
+from .cw import CW
+from .hab import HAB
+from .spp import SPP
+from .bd import BD
+
+__all__ = [
+    "FIS",
+    "MAR",
+    "FP",
+    "AO",
+    "NP",
+    "CS",
+    "CP",
+    "TR",
+    "LIV",
+    "ECO",
+    "LE",
+    "ICO",
+    "LSP",
+    "SP",
+    "CW",
+    "HAB",
+    "SPP",
+    "BD",
+]
